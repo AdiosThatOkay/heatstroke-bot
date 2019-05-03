bc7d0427500dd2d3b4298070d473b01f35d0c086
AdiosThatOkay <borntoloseandlivetowin@gmail.com>
2019-05-03 22:43:53 +0900

    Fix config.py db.py

--- hsbot/config.py
+++ hsbot/config.py
@@ -3,7 +3,8 @@ import os
 SQLALCHEMY_DATABASE_URI = 'sqlite:///database/heatstroke.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-LINE_CHANNEL_SECRET = os.environ['LINE_CHANNEL_SECRET']
-
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN',
+                                           'DUMMY_TOKEN')
+LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET',
+                                     'DUMMY_SECRET')
 DEBUG = True
--- hsbot/scripts/db.py
+++ hsbot/scripts/db.py
@@ -21,13 +21,14 @@ class InsertCSV(Command):
     def run(self):
         csv_path = os.path.join(app.root_path,
                                 'database/all_observatories_2019.csv')
-        reader = csv.reader(open(csv_path))
-        for row in reader:
-            if reader.line_num == 1:
-                continue
-            observatory = Observatory(
+        with open(csv_path) as csv_obj:
+            reader = csv.reader(csv_obj)
+            for row in reader:
+                if reader.line_num == 1:
+                    continue
+                observatory = Observatory(
                     code=row[1], pref=row[0], name=row[3], name_kana=row[4],
                     location=row[5], lat_deg=row[6], lat_min=row[7],
                     lon_deg=row[8], lon_min=row[9])
-            db.session.add(observatory)
-        db.session.commit()
+                db.session.add(observatory)
+            db.session.commit()
